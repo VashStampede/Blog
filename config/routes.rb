c612cbc904ed2ec71f5293317660746ddd1127fa
VashStampede <worldmader@mail.ru>
2014-02-13 15:37:07 +0200

    для миграции на херо

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
-Blog::Application.routes.draw do
-  resources :posts
-  root :to => "welcome#index"
-end
+  Blog::Application.routes.draw do
+    resources :posts
+    root :to => "welcome#index"
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
