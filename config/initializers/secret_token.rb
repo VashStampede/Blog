713f7d2f52cd8226276636bf50d8d5953b6cedc2
VashStampede <worldmader@mail.ru>
2014-02-18 16:34:10 +0200

    Add: login system

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'a59df39acbea29a27269e7cc310f84f279e8c10f86dcc8c4f91196bdbbc7e861307f87ca5a2e6fbdd5001f29dc258a0b76738b253cfc9edcab7b3d1e31b25da8'
