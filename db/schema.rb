1b0da1bf31b6de0d92f4eb96955dfff371b50d98
VashStampede <worldmader@mail.ru>
2014-03-19 07:54:07 +0200

    add:that`s all1

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -40,8 +40,8 @@ ActiveRecord::Schema.define(:version => 20140227095154) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "name"
-    t.string   "surname"
+    t.text     "name"
+    t.text     "surname"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
